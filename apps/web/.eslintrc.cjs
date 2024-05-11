e0d497dec31583d77a22c2190ac0859183923fa0
Michael J. Roberts <michael@observerly.com>
2024-05-11 13:33:17 +0100

    revert: add initial workspace configuration and app setup
    revert: add initial workspace configuration and app setup

--- apps/web/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-const config = {
-  root: true,
-  extends: ["@koroutine/eslint-config-custom"],
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-module.exports = config;
