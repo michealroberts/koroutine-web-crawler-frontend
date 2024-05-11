063a551ae472084917ee9a407388fdf347ceec3c
Michael J. Roberts <84131395+michealroberts@users.noreply.github.com>
2024-05-11 13:27:15 +0100

    chore: add initial workspace configuration and app setup (#5)
    chore: add initial workspace configuration and app setup

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  extends: ["@koroutine/eslint-config-custom"],
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+module.exports = config;
